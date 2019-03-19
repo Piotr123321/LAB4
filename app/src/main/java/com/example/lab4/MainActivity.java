b71cdc5985843d2b55c19da4fdcf46aea2064c8a
Piotr123321 <plewa7@o2.pl>
2019-03-19 21:40:44 +0100

    dodanie listy do wyswietlenia

--- app/src/main/java/com/example/lab4/MainActivity.java
+++ app/src/main/java/com/example/lab4/MainActivity.java
@@ -2,16 +2,31 @@ package com.example.lab4;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<String> target;
+    private ArrayAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_main );
 
-        // to jest komentarz do GIT la la la la la
-        // a to ciag dalszy komentarza
-        // no to teraz cos znoabczymy
+        String[] values = new String[] {"Pies", "Kot", "Koń", "Gołąb", "Krók", "Dzik", "Karp",
+                "Osioł", "Chomik", "Mysz", "Jeż", "Karaluch"};
+
+        this.target=new ArrayList<String>();        //umieszczenie elenetow z tablicy
+        this.target.addAll(Arrays.asList(values));
+
+        this.adapter=new ArrayAdapter(this,android.R.layout.simple_list_item_1,this.target); //inicjalizacja listy
+
+        ListView listview = (ListView) findViewById(R.id.listView);
+        listview.setAdapter(this.adapter);
     }
 }
