20dd4b66c36f2db164235ddbc512e5a71a0e7642
Piotr123321 <plewa7@o2.pl>
2019-03-20 00:37:03 +0100

    dodawanie wpisu do bazy danych

--- app/src/main/java/com/example/lab4/MainActivity.java
+++ app/src/main/java/com/example/lab4/MainActivity.java
@@ -59,9 +59,12 @@ public class MainActivity extends AppCompatActivity {
         if(requsetCode==1 && resultCode==RESULT_OK)
         {
             Bundle extras = data.getExtras();
-            String nowy = (String)extras.get("wpis");
-            target.add(nowy);
+            Animal nowy=(Animal) extras.getSerializable( "nowy" );
+
+            this.db.dodaj(nowy);
+            adapter.changeCursor( db.lista() );
             adapter.notifyDataSetChanged();
+
         }
 
     }
