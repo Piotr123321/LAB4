5004fd1e59f61eef5daff7b4a7dc6068d2fba9c7
Piotr123321 <plewa7@o2.pl>
2019-03-19 20:27:59 +0100

    dodano komentarz

--- app/src/main/java/com/example/lab4/MainActivity.java
+++ app/src/main/java/com/example/lab4/MainActivity.java
@@ -9,5 +9,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_main );
+
+        // to jest komentarz do GIT la la la la la
+        // a to ciag dalszy komentarza
     }
 }
